f94a53c0b26f1d322b8799ab79a3777bab22f9e1
jennetmagomedova <jimagomedova@gmail.com>
2017-10-02 16:04:07 +0300

    Initial commit.

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-window.onload=function() {
-    var amount = prompt("На какую сумму хотите взять кредит?");
-    var period = prompt("На сколько месяцев?");
-    var rate = prompt("По какой процентной ставке?") / 1200;
-    var month = prompt("С какого месяца(в формате ХХ)?");
-    var year = prompt("С какого года?");
+function CalculateCredit() {
+    var amount = document.getElementById('amount').value;
+    var period = document.getElementById('period').value;
+    var rate = document.getElementById('rate').value / 1200;
+    var month = document.getElementById('month').value;
+    var year = document.getElementById('year').value;
     var paymentAmount = amount * (rate * Math.pow(1 + rate, period)) / (Math.pow(1 + rate, period) - 1);
     period++;
     var t = document.createElement('table');
     var tbody = document.createElement('tbody');
     for (var i = 0; i < period; i++) {
         var tr = document.createElement('tr');
-        for (var j = 0; j<8; j++) {
+        for (var j = 0; j<7; j++) {
             var td = document.createElement('td');
-            if ((j%7)==0){
-                btn=document.createElement('input');
-                btn.type="button";
-                btn.onclick=function () {
-                    document.getElementById('modal').style.display="block";
-                    document.getElementById('overlay').style.display="block";
-                }
-                btn.value="Добавить платеж";
-                td.appendChild(btn);
-            }
             tr.appendChild(td);
-            td.id = "td";
         }
         tbody.appendChild(tr);
     }
@@ -36,7 +25,6 @@ window.onload=function() {
     t.rows[0].cells[4].innerHTML="Основной долг";
     t.rows[0].cells[5].innerHTML="Начисленные проценты";
     t.rows[0].cells[6].innerHTML="Платеж";
-    t.rows[0].cells[7].innerHTML=" ";
     t.rows[1].cells[3].innerHTML=amount;
     for (i=1;i<period;i++){
       t.rows[i].cells[0].innerHTML=i;
@@ -53,10 +41,6 @@ window.onload=function() {
     }
     for(i=2;i<period;i++){
       t.rows[i].cells[3].innerHTML=(t.rows[i-1].cells[3].innerHTML-t.rows[i].cells[4].innerHTML).toFixed(3);
-    }
-    document.getElementById("closeModal").onclick=function () {
-        document.getElementById('modal').style.display="none";
-        document.getElementById('overlay').style.display="none";
     }
 }
 
